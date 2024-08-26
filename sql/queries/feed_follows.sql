dcd1eba11fe1b837577a12781d61ae324ae7a390
Chad <chad1020@gmail.com>
2024-08-26 17:06:50 -0500

    added del

--- sql/queries/feed_follows.sql
+++ sql/queries/feed_follows.sql
@@ -3,3 +3,6 @@ INSERT INTO feed_follows (id, created_at, updated_at, user_id, feed_id) VALUES (
 
 -- name: GetFeedFollows :many
 SELECT * FROM feed_follows WHERE user_id = $1;
+
+-- name: DeleteFeedFollow :exec
+DELETE FROM feed_follows WHERE id = $1 AND user_id = $2;
